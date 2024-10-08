5cd09048d70549e3ce670a97b6ecb498382edd6d
NNNnatTTT <nathan.ng777@gmail.com>
2024-10-08 23:37:06 +0800

    Added login page AdminController.java

--- src/main/java/cs204/project/Controller/AdminController.java
+++ src/main/java/cs204/project/Controller/AdminController.java
@@ -1,5 +1,17 @@
-package cs204.project.Controller;
+package cs204.project.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/admin")
 public class AdminController {
-    
+
+    @GetMapping("")
+    @ResponseBody
+    public String getDashboard(){
+        return "This is admin dashboard";
+    }
 }
