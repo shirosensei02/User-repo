881aa191090d9b0404d85466919a677a72baa61d
NNNnatTTT <nathan.ng777@gmail.com>
2024-10-13 01:37:55 +0800

    pom.xml errors in building

---
+++ src/main/java/cs204/project/Model/playerrank/playerrank.java
@@ -0,0 +1,31 @@
+// package cs204.project.model.playerrank;
+
+// import jakarta.persistence.*;
+
+
+// @Entity
+// @Table(name = "playerrank")
+// public class playerrank {
+//     @Id
+//     @OneToOne
+//     @JoinColumn(name = "player_id")
+//     private Long id;
+
+//     private int rank;
+
+//     public Long getId() {
+//         return id;
+//     }
+//     public void setId(Long id) {
+//         this.id = id;
+//     }
+//     public int getRank() {
+//         return rank;
+//     }
+//     public void setRank(int rank) {
+//         this.rank = rank;
+//     }
+    
+    
+//     // private Enum region:
+// }
