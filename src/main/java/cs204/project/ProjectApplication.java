1fe5b0c386c8fbc73b687479583026015387ce3e
Shirosensei <yeomingen02@gmail.com>
2024-10-28 14:26:19 +0800

    db password env

--- src/main/java/cs204/project/ProjectApplication.java
+++ src/main/java/cs204/project/ProjectApplication.java
@@ -8,18 +8,30 @@ import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class ProjectApplication {
 
-	public static void main(String[] args) {
-		// Start the Spring Boot application
-		SpringApplication.run(ProjectApplication.class, args);
-    //     Dotenv dotenv = Dotenv.configure().load();
-    // System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+  public static void main(String[] args) {
+    //
+    Dotenv dotenv = Dotenv.configure().load();
+    System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
-
-		// No tournament-related operations as we are focusing on login page testing
-
-		// You can add any test logic or bean checks for the login page here if necessary
-	}
-
+    ApplicationContext ctx = SpringApplication.run(ProjectApplication.class, args);
+  }
 }
-            
-            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
